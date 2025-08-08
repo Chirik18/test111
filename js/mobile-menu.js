8885cd1f94d953c915febac25817fbabb73ec320
Chirik18 <cherniavskyi0729@gmail.com>
2025-08-08 09:11:30 +0300

    Update index.html

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close')
-const toggleModal = () => mobileMenu.classList.toggle('is-open');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
